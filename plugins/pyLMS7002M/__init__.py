d290f46245cf324cb9cd09d78125e8e2ab80e3e9
Bogdan Vacaliuc <vacaliucb@ornl.gov>
2017-04-23 23:47:12 -0400

    comment out import * and explain why

--- plugins/pyLMS7002M/__init__.py
+++ plugins/pyLMS7002M/__init__.py
@@ -1,3 +1,4 @@
-from LMS7002_EVB import *
-from LimeSDR import *
-
+#from LMS7002_EVB import *
+#from LimeSDR import *
+# NOTE: these cause problems with importing modules from subfolders
+# http://stackoverflow.com/questions/8953844/import-module-from-subfolder
