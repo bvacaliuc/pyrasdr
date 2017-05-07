6f38bf0d243409fc3385959f9bf98dfab0a8bcbc
Bogdan Vacaliuc <vacaliucb@ornl.gov>
2017-05-07 18:05:45 -0400

    is this what I want?

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,9 @@
+import pytest
 
 def test():
-    from plugins import LMS7002, ADF4002, Si5351, Proxy
+    with pytest.warns(None) as record:
+        from plugins import LMS7002, ADF4002, Si5351, Proxy
+    return True
+
+if __name__ == '__main__':
+    assert test()
